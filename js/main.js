c8b7a91b736191e47acaf7a674202514d90a6024
Hien Nguyen <hnguy179@gmail.com>
2021-04-15 11:26:05 -0700

    Created a GET request in getDishData function and passed in 2 parameters. #1

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function getDishData(mealType, mainIngredient) {
+  var xhrObj = new XMLHttpRequest();
+  xhrObj.open('GET', 'https://api.edamam.com/search?app_id=56d29915&app_key=8a2c94a84e7c4a5a2c94ff997508b44b&from=0&to=3&calories=591-722&health=alcohol-free&mealType=' + mealType + '&q=' + mainIngredient);
+  xhrObj.responseType = 'json';
+  xhrObj.addEventListener('load', function () {
+    console.log(xhrObj.status);
+    console.log(xhrObj.response);
+  });
+  xhrObj.send();
+}

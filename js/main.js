377e766ca108f125ed875633c408bbce2d4268ea
Hien Nguyen <hnguy179@gmail.com>
2021-04-15 22:37:38 -0700

    Listened for submit event on form 1 and stored clicked value as an argument. #1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
-function getDishData(mealType, mainIngredient) {
+var $mealTypeForm = document.getElementById('meal-type-form');
+var $mealType = document.getElementsByName('meal-type');
+
+function getRecipeData(mealType) {
   var xhrObj = new XMLHttpRequest();
-  xhrObj.open('GET', 'https://api.edamam.com/search?app_id=56d29915&app_key=8a2c94a84e7c4a5a2c94ff997508b44b&from=0&to=3&calories=591-722&health=alcohol-free&mealType=' + mealType + '&q=' + mainIngredient);
+  xhrObj.open('GET', 'https://api.edamam.com/search?app_id=56d29915&app_key=8a2c94a84e7c4a5a2c94ff997508b44b&q=&mealType=' + mealType);
   xhrObj.responseType = 'json';
   xhrObj.addEventListener('load', function () {
-    console.log(xhrObj.status);
-    console.log(xhrObj.response);
+    console.log('xhrstatus', xhrObj.status);
+    console.log('xhresponse', xhrObj.response);
   });
   xhrObj.send();
 }
 
-getDishData('breakfast', 'chicken');
+function submitAnswers(event) {
+  event.preventDefault();
+  var mealType = null;
+  if (event.target.getAttribute('id') === 'meal-type-form') {
+    for (var i = 0; i < $mealType.length; i++) {
+      if ($mealType[i].checked) {
+        mealType = $mealType[i].getAttribute('id');
+      }
+    }
+    getRecipeData(mealType);
+  }
+}
+
+$mealTypeForm.addEventListener('submit', submitAnswers);
